2aaf71d19b0f05ef73d5f2bfdcda8e53e7dcf09b
Philippe Marschall <philippe.marschall@gmail.com>
2020-05-02 18:15:45 +0200

    wording

--- src/main/java/com/github/marschall/directorykeystore/DirectoryKeystoreProvider.java
+++ src/main/java/com/github/marschall/directorykeystore/DirectoryKeystoreProvider.java
@@ -10,12 +10,16 @@ public final class DirectoryKeystoreProvider extends Provider {
   public static final String NAME = "directory";
 
   /**
-   * The type of this keystore provider that uses directories to store certificates.
+   * The type of keystore that uses directories to store certificates.
    */
   public static final String TYPE = "directory";
 
+  /**
+   * Default constructor, either called directly by programmatic registration or
+   * by JCA.
+   */
   public DirectoryKeystoreProvider() {
-    super(NAME, 0.1d, "directory (KeyStore and CertStore)");
+    super(NAME, 0.1d, "directory (KeyStore)");
     this.put("KeyStore." + TYPE, DirectoryKeystore.class.getName());
     this.put("CertStore." + TYPE, DirectoryCertStore.class.getName());
   }
