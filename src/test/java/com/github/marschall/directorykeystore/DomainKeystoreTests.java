331307d4662df9bc442e11ed9857d62c36889987
Philippe Marschall <philippe.marschall@gmail.com>
2021-05-25 18:46:05 +0200

    add dks tests

---
+++ src/test/java/com/github/marschall/directorykeystore/DomainKeystoreTests.java
@@ -0,0 +1,38 @@
+package com.github.marschall.directorykeystore;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.DomainLoadStoreParameter;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.KeyStore.LoadStoreParameter;
+import java.security.KeyStore.ProtectionParameter;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+class DomainKeystoreTests {
+
+  @Test
+  void load() throws GeneralSecurityException, IOException, URISyntaxException {
+    KeyStore keyStore = KeyStore.getInstance("DKS");
+    URI dksUri = new URI(DomainKeystoreTests.class.getClassLoader().getResource("sample-keystore/dks/sample.dks").toExternalForm() + "#app1");
+    Map<String, ProtectionParameter> protectionParams = Collections.emptyMap();
+    LoadStoreParameter loadStoreParameter = new DomainLoadStoreParameter(dksUri, protectionParams);
+    keyStore.load(loadStoreParameter);
+
+    assertTrue(keyStore.isCertificateEntry("cert")); // from the directory truststore
+
+    List<String> aliases = Collections.list(keyStore.aliases());
+    assertThat(aliases, hasSize(greaterThan(10))); // from the JDK truststore
+  }
+
+}
